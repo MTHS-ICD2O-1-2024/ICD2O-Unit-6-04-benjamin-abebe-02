388616e5c791cdad82939ae0d0e45843d0ff2425
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-05-22 17:10:39 +0000

    innit

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+
+"use strict"
+
+window.onload = function () {
+  // calculates the volume of a sphere
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get("radius")
+  console.log(radius)
+
+  // process
+  const volumeFormula = (4 / 3) * Math.PI * radius ** 3
+  const volume = Math.round(volumeFormula * 100) / 100
+
+  // display results
+  document.getElementById("radius").innerHTML = "<p> Radius is: " + radius + " .</p>"
+  document.getElementById("area").innerHTML = "<p> Volume is: " + volume + " cm³"
+}
